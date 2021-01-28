792481f9f754816b31b8fc453af76fe73846e4c5
vtendero <tendero.valle@gmail.com>
2021-01-28 09:45:15 +0100

    add random number to js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const inputElement = document.querySelector('.js-number');
+const submitElement = document.querySelector('.js-submit');
+const hintElement = document.querySelector('js-hint');
+const triesElement =document.querySelector('.js-tries');
+
+
+//Número aleatorio 
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+}
+
+let randomNumber = getRandomNumber(100);
+console.log('El número aleatorio es ' + randomNumber);
+
