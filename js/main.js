d1626cca17295377342e7a8987928bd5182dd7fa
vtendero <tendero.valle@gmail.com>
2021-01-28 10:41:04 +0100

    add inputNumber function

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 const inputElement = document.querySelector('.js-number');
 const buttonElement = document.querySelector('.js-button');
-const hintElement = document.querySelector('js-hint');
+const hintElement = document.querySelector('.js-hint');
 const triesElement =document.querySelector('.js-tries');
 
 
@@ -15,8 +15,27 @@ let randomNumber = getRandomNumber(100);
 console.log('El número aleatorio es ' + randomNumber);
 
 
-function handlerButton () {
-    console.log(2);
-}
+//Número introducido
+function handlerButton (event) {
+    inputNumber ();
+};
 
-buttonElement.addEventListener('click', handlerButton);
+buttonElement.addEventListener('click',handlerButton);
+
+
+//Pista
+function inputNumber () {
+    const inputElementValue = parseInt(inputElement.value);
+    console.log('Número introducido ' + inputElementValue);
+    if (inputElementValue === randomNumber) {
+        hintElement.innerHTML = '¡Enhorabuena! Has acertado campeona';
+    } else if (inputElementValue < 1  || inputElementValue > 100) {
+        hintElement.innerHTML = 'El número debe estar entre 1 y 100';
+    } else if (inputElementValue < randomNumber) {
+        hintElement.innerHTML = 'Demasiado bajo ¡sigue intentándolo!';
+    } else if (inputElementValue > randomNumber) {
+        hintElement.innerHTML = 'Demasiado alto ¡sigue intentándolo!';
+    } else {
+        hintElement.innerHTML ='Introduce un número para comenzar a jugar';
+    }
+}
